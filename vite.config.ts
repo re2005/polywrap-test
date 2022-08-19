0f2e705695d05574d663f7b33b070169098ea1cd
cbrzn <cesarbrazon10@gmail.com>
2022-08-19 16:54:28 +0200

    chore: add alias for url

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
+            url: 'rollup-plugin-node-polyfills/polyfills/url',
         },
     },
     build: {
